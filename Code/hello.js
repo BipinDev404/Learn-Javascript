bf1d29eef538facc78884ee2dcbd2707176edc8e
Bipin Yadav <bipinfordev@gmail.com>
2025-06-14 13:16:42 +0530

    Update message

---
+++ Code/hello.js
@@ -0,0 +1,5 @@
+ console.log("Hello World");
+ console.log("I am BIPIN Yadav");
+ console.log("and I love to do code");
+
+ //oh , my first languages is c and where always write \n to go to next lines and here fucking no need to write automatic took to next line where i write new console
